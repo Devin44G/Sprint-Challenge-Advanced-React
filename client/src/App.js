1acedffd054fed8ab00ec8e9906de016f1231ff1
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-02-14 11:55:46 -0600

    Simple render of date to screen -> Made and implemented a useLocalStorage custom hook so value in input field remains on refresh

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Data from './components/Data';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome</h1>
+      <h1>Women's World Cup Player</h1>
+      <Data />
     </div>
   );
 }
